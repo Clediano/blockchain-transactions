14c21994afc3ef7b5dee3c4f1753b9b4e3c1c17b
Clediano Estefenon <clediano@microsys.inf.br>
2020-01-21 13:40:06 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,4 @@
+const { port, endpoint, masterKey } = require('./src/config/secret');
+console.log(`Your port is ${port}`);
+console.log(`Your endpoint is ${endpoint}`);
+console.log(`Your masterKey is ${masterKey}`);
